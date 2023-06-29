3d77faeb8b3983ec43c922886b82f05ea6629809
OukiLove <oukilove76@gmail.com>
2023-06-29 18:03:08 +0600

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,13 +49,7 @@ function onYouTubeIframeAPIReady() {
   if (videoId) {
     window.player = new YT.Player('player', {
       videoId: videoId,
-      playerVars: {
-        autoplay: 1,
-        loop: 1,
-        controls: 0,
-        showinfo: 0,
-        mute: 0
-      },
+      playerVars: { 'autoplay': 0, 'controls': 0, 'loop': 1, 'playlist': videoId },
       events: {
         onReady: (event) => {
           event.target.setVolume(15);
